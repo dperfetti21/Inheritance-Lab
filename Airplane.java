bb1ac6ea161867b155c608ecc1c7e3bc2ba71177
declinda <dperfetti21@scarsdaleschools.org>
2020-02-11 10:46:40 -0500

    Initial sharing of project

---
+++ Airplane.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Airplane here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Airplane implements Flier
+{
+ 
+ public void fly()
+ {
+     System.out.println("Using airplane wings to fly");
+ }
+    
+}
---
+++ Bird.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Bird here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Bird implements Flier
+{
+  
+   public void fly()
+   {
+       System.out.println("Using wings to fly");
+   }
+}
